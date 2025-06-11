5fbf321fa3e2736fa4992239954f8a825e299522
EJGoose <solangoose@duck.com>
2025-06-11 21:58:09 +0100

    Update ManageStudentInfoDB.sql
    merge files

--- ManageStudentInfoDB.sql
+++ ManageStudentInfoDB.sql
@@ -71,3 +71,75 @@ GRANT SELECT, INSERT
 ON Student_Info
 TO student_role
 
+
+
+-- Create a view named student_overview that shows the ID, name, and grade columns from the student's table.
+CREATE VIEW Student_Overview AS
+SELECT StudentID, FirstName, LastName, Grade
+FROM Student_Info;
+
+-- Query the student_overview view to verify it displays the correct data.
+SELECT * FROM Student_Overview; 
+
+-- Modify the view to include a calculated field that shows age categorised as 'Minor' if the age is less than 18, and 'Adult' otherwise.
+DROP VIEW IF EXISTS Student_Overview;
+
+CREATE VIEW Student_Overview AS
+SELECT 
+    StudentID, 
+    FirstName, 
+    LastName, 
+    Grade,
+    CASE  
+        WHEN Age >= 18 THEN 'Adult';
+        ELSE 'Minor';
+    END AS age_category
+FROM Student_Info;
+SELECT * FROM Student_Overview;
+
+  
+
+-- Create a stored procedure named add_student that takes the name, age, and grade as parameters and inserts a new record into the students table.
+CREATE PROCEDURES add_student
+(IN stud_FirstName VARCHAR(50), IN stud_LastName VARCHAR(50), IN stud_Age INT, IN stud_Grade FLOAT)
+
+BEGIN
+	INSERT INTO Student_Info(FirstName, LastName, Age, Grade)
+    VALUES (stud_FirstName, stud_LastName, stud_Age, stud_Grade);
+END;
+
+-- Run the stored procedure to add a new student.
+CALL add_student('Linda', 'Haynes', 12, 3.87);
+
+-- Modify the stored procedure to return the id of the newly added student after insertion.
+DROP PROCEDURE IF IT EXISTS add_student;
+
+CREATE PROCEDURE add_student(IN stud_FirstName VARCHAR(50), IN stud_LastName VARCHAR(50), IN stud_Age INT, IN stud_Grade FLOAT, OUT student_id INT)
+
+BEGIN
+    INSERT INTO Student_Info(FirstName, LastName, Age, Grade)
+    VALUES (stud_FirstName, stud_LastName, stud_Age, stud_Grade);
+
+    SET student_id = LAST_INSERT_ID();
+END;
+
+-- Verify that the stored procedure works as expected.
+SET @student_id=0;
+CALL add_student('John', 'Smyth', 14, 2.80, @student_id);
+SELECT @student_id; 
+
+
+
+-- Create a user-defined function named calculate_discount that takes a price and a discount percentage as input and returns the discounted price.
+
+CREATE FUNCTION calculate_discount (price DECIMAL(10,2), disccount DECIMAL(5,2))
+RETURN DECIMAL(10,2)
+BEGIN
+	RETURN price - (price * discount/100);
+END;
+
+
+--Write a query to test the function by calculating the discounted price for an item with a price of 100 and a discount of 15%.
+
+SELECT calculate_discount(100, 15) AS discounted_price;
+
